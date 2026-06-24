6618ce1316c4f4074f14596dc83112be34f454eb
Ahri <sexyahri@qq.com>
2026-06-25 00:20:37 +0800

    shorten alembic revision id (#137)
    Co-authored-by: Ahri <whoreahri@gamil.com>

--- alembic/versions/0009_event_importance.py
+++ alembic/versions/0009_event_importance.py
@@ -1,6 +1,6 @@
 """Add finance event importance assessment fields.
 
-Revision ID: 0009_add_finance_event_importance
+Revision ID: 0009_event_importance
 Revises: 0008_task_trigger_source
 Create Date: 2026-06-24
 """
@@ -12,14 +12,13 @@ from typing import Sequence, Union
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "0009_add_finance_event_importance"
+revision: str = "0009_event_importance"
 down_revision: Union[str, Sequence[str], None] = "0008_task_trigger_source"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.execute("ALTER TABLE alembic_version ALTER COLUMN version_num TYPE VARCHAR(255)")
     op.execute("ALTER TABLE finance_events ADD COLUMN IF NOT EXISTS importance_score INTEGER")
     op.execute("ALTER TABLE finance_events ADD COLUMN IF NOT EXISTS importance_reason TEXT")
     op.execute("ALTER TABLE finance_events ADD COLUMN IF NOT EXISTS importance_confidence FLOAT")
